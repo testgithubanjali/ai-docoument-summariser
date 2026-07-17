affcf3039f76c406a38245ad352e887a7027e1b2
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-17 18:53:26 +0530

    update: updated the routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,14 +2,26 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
+
+	"github.com/testgithubanjali/ai-document-summarizer/internal/handlers"
+	"github.com/testgithubanjali/ai-document-summarizer/internal/repository"
+	"github.com/testgithubanjali/ai-document-summarizer/internal/service"
 )
 
 func SetupRoutes(router *gin.Engine) {
 
+	// Health Check
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "AI Document Summarizer API is running",
 		})
 	})
 
+	// Dependency Injection
+	userRepo := repository.NewUserRepository()
+	userService := service.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userService)
+
+	// User Routes
+	router.POST("/register", userHandler.Register)
 }
