0ff47cb98699e6b992eca801ad0b01bab9e5006f
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-18 21:52:35 +0530

    update: updated the dto document

--- internal/dto/document.go
+++ internal/dto/document.go
@@ -1,8 +1,9 @@
 package dto
 
-type UploadResponse struct {
+type ProcessDocumentResponse struct {
 	ID       uint   `json:"id"`
 	FileName string `json:"file_name"`
 	FileType string `json:"file_type"`
+	Summary  string `json:"summary"`
 	Message  string `json:"message"`
 }
