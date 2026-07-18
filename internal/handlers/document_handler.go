23a59fa587f815eb2082f447604436fea9db1e1b
testgithubanjali <131359262+testgithubanjali@users.noreply.github.com>
2026-07-18 21:30:11 +0530

    add: added code inside document handler

---
+++ internal/handlers/document_handler.go
@@ -0,0 +1,86 @@
+package handlers
+
+import (
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/testgithubanjali/ai-document-summarizer/internal/dto"
+	"github.com/testgithubanjali/ai-document-summarizer/internal/models"
+	"github.com/testgithubanjali/ai-document-summarizer/internal/service"
+	"github.com/testgithubanjali/ai-document-summarizer/internal/utils"
+)
+
+type DocumentHandler struct {
+	documentService *service.DocumentService
+}
+
+func NewDocumentHandler(service *service.DocumentService) *DocumentHandler {
+	return &DocumentHandler{
+		documentService: service,
+	}
+}
+
+func (h *DocumentHandler) Upload(c *gin.Context) {
+
+	userIDValue, exists := c.Get("user_id")
+	if !exists {
+		utils.Error(c, http.StatusUnauthorized, "Unauthorized")
+		return
+	}
+
+	userID := uint(userIDValue.(float64))
+
+	file, err := c.FormFile("document")
+	if err != nil {
+		utils.Error(c, http.StatusBadRequest, "Document is required")
+		return
+	}
+
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+
+	if ext != ".pdf" && ext != ".docx" {
+		utils.Error(c, http.StatusBadRequest, "Only PDF and DOCX files are allowed")
+		return
+	}
+
+	err = os.MkdirAll("uploads", os.ModePerm)
+	if err != nil {
+		utils.Error(c, http.StatusInternalServerError, "Failed to create upload directory")
+		return
+	}
+
+	fileName := strconv.FormatInt(time.Now().UnixNano(), 10) + ext
+	filePath := filepath.Join("uploads", fileName)
+
+	if err := c.SaveUploadedFile(file, filePath); err != nil {
+		utils.Error(c, http.StatusInternalServerError, "Failed to save file")
+		return
+	}
+
+	document := models.Document{
+		UserID:   userID,
+		FileName: file.Filename,
+		FilePath: filePath,
+		FileType: ext,
+	}
+
+	if err := h.documentService.Create(&document); err != nil {
+		utils.Error(c, http.StatusInternalServerError, "Failed to save document")
+		return
+	}
+
+	response := dto.UploadResponse{
+		ID:       document.ID,
+		FileName: document.FileName,
+		FileType: document.FileType,
+		Message:  "Document uploaded successfully",
+	}
+
+	utils.Success(c, http.StatusCreated, response)
+}
